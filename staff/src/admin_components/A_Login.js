58bacd243edc070c1937998cac8d3c2c6825965a
Punith A <punith@greentiger.in>
2026-05-09 17:37:24 +0530

    Fixed profile API

--- staff/src/admin_components/A_Login.js
+++ staff/src/admin_components/A_Login.js
@@ -16,12 +16,15 @@ const A_Login = () => {
 
     try {
       const response = await axios.post(
-        "https://staffleavehub-production.up.railway.app/login",
-        {
-          email_id: email,
-          password: password,
-        }
-      );
+  "https://staffleavehub-production.up.railway.app/login",
+  {
+    email_id: email,
+    password: password,
+  },
+  {
+    withCredentials: true
+  }
+);
 
       if (response.status === 200) {
         setMessage("Login successful");
